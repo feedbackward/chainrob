54e7de371042912e8dee5966044e70b64e90ecfa
mjh <matthew-h@ids.osaka-u.ac.jp>
2018-12-07 15:07:44 +0900

    Revised with simplified z-score calibration.

--- config.py
+++ config.py
@@ -8,7 +8,3 @@ LAM_MIN = 1e-3 # safe minimum value for lambda (see demo.ipynb notebook).
 SIGMA_MIN = 1e-4 # safe minimum value for sigma (see demo.ipynb notebook).
 CONF_DELTA = 0.005 # Confidence parameter delta (see demo.ipynb notebook).
 
-CALIB_S0 = 10.0 # Calibration factor.
-CALIB_THRES = 0.75 # Decision threshold.
-
-
--- robustify.py
+++ robustify.py
@@ -16,62 +16,26 @@ def softmean(x, paras=None):
     Catoni and Giulini (2017) key insights applied
     to a one-dimensional setting, very handy.
     
-    NOTE: "calib" refers to a sort of calibration
-    that we do when setting the v-bound.
-    No additional factors here.
-    
     Here "x" is assumed to be an ndarray with
     no structure, just (k,) shape.
     '''
 
-    # A couple statistics.
     est_mean = np.mean(x)
     est_var = max(np.var(x), 0.001)
-    
-    # Checks.
-    calib_shift = (abs(est_mean) / est_var) > config.CALIB_THRES
-    calib_scale = not calib_shift
+    est_sd = math.sqrt(est_var)
 
-    # If the variance is not much smaller than the absolute
-    # position, then temporarily downscale, do estimation, and
-    # then return to original scale.
-    if calib_scale:
-        Tbound = (est_var + est_mean**2) / config.CALIB_S0
-        Tbound_safe = max(Tbound, config.T_MIN)
-        vbound_safe = Tbound_safe
-        lam = math.sqrt(2*math.log(1/config.CONF_DELTA)/(x.size*vbound_safe))
-        beta = math.sqrt(
-            2*Tbound_safe*math.log(1/config.CONF_DELTA)/vbound_safe
-        )
-        xhat = hlp.est_robust(x=x/config.CALIB_S0,
-                              lam=lam, beta=beta) * config.CALIB_S0
-
-    # If the variance is much smaller than the position, then a
-    # simple temporary shift will be sufficient to combat bias.
-    elif calib_shift:
-        Tbound = est_var + est_mean**2
-        Tbound_safe = max(Tbound, config.T_MIN)
-        vbound_safe = Tbound_safe
-        lam = math.sqrt(2*math.log(1/config.CONF_DELTA)/(x.size*vbound_safe))
-        beta = math.sqrt(
-            2*Tbound_safe*math.log(1/config.CONF_DELTA)/vbound_safe
-        )
-        xhat = hlp.est_robust(x=(x-est_mean),
-                              lam=lam, beta=beta) + est_mean
-
-    else:
-        Tbound = est_var + est_mean**2
-        Tbound_safe = max(Tbound, config.T_MIN)
-        vbound_safe = Tbound_safe
-        lam = math.sqrt(2*math.log(1/config.CONF_DELTA)/(x.size*vbound_safe))
-        beta = math.sqrt(
-            2*Tbound_safe*math.log(1/config.CONF_DELTA)/vbound_safe
-        )
-        xhat = hlp.est_robust(x=x, lam=lam, beta=beta)
+    # Treat data as z-scores, thus no need for "safe" bounds.
+    Tbound = 1.0 
+    vbound = Tbound
+    lam = math.sqrt(2*math.log(1/config.CONF_DELTA)/(x.size*vbound))
+    beta = math.sqrt(
+        2*Tbound*math.log(1/config.CONF_DELTA)/vbound
+    )
+    xhat = hlp.est_robust(x=(x-est_mean)/est_sd, lam=lam, beta=beta)
+    xhat *= est_sd
+    xhat += est_mean
 
     return xhat
-
-
 
 
 
